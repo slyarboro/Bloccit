8d7af6ce546bc36327963d8d44b9c10f655b7943
Stephanie Yarboro <stephanieyarboro@gmail.com>
2016-09-27 15:26:28 -0400

    Assignment-31crud: Regrown

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,13 +9,14 @@ RSpec.describe PostsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
+
       expect(response).to have_http_status(:success)
     end
 
-
     it "assigns [my_post] to @posts" do
       get :index
-      expect(assigns(:posts)).to eq([my_post])
+
+      expect(assigns(:post)).to eq([my_post])
     end
   end
 
