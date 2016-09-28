fb1ac5f78b731c5bf023a49d8c18fd28e04cf0ce
Stephanie Yarboro <stephanieyarboro@gmail.com>
2016-09-28 15:16:40 -0400

    Assignment-33crudmas

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,64 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    # @question = Question.new(params.require(:question).permit(:title, :body, :resolved))
+
+    if @question.save
+      flash[:notice] = "Question saved successfully."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving your question. Try again, if you please!"
+      render :new
+    end
+  end
+
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+
+  def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    if @question.save
+    # if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
+      flash[:notice] = "Question updated successfully."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving your question. Try again, if you wish!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\ was deleted successfully."
+      redirect_to questions_path
+    else
+      flash[:error] = "Your question was unable to be deleted."
+      render :show
+    end
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  context "attributes" do
+    let(:question) { Question.create!(title: "Quest-ion Title", body: "Quest-atom Body", resolved: false) }
+
+    it "responds to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "responds to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "responds to resolved" do
+      expect(question).to respond_to(:resolved)
+    end
+
+  end
+end
