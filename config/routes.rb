79180fffade3dc6d19076105afaf1ffd3b9344f0
Stephanie Yarboro <stephanieyarboro@gmail.com>
2016-10-03 05:16:40 -0400

    Assignment-37signing_up: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+
+  resources :users, only: [:new, :create]
+  post 'confirm' => 'users#confirm'
+  get 'confirm' => 'users#new'
+  
   resources :topics do
     resources :posts, except: [:index]
   end
-
-# create routes for #new and #create actions
-# [only:] prevents Rails creating unnecessary routes
-  resources :users, only: [:new, :create]
 
 
   get 'about' => 'welcome#about'
