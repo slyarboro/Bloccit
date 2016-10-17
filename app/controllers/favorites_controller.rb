df5cf40ca09c13a80ce69ab6d1dd2a927231fbca
Stephanie Yarboro <stephanieyarboro@gmail.com>
2016-10-17 12:53:33 -0400

    CP-44favorites: Completed

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,32 @@
+class FavoritesController < ApplicationController
+   # redirect - guest users must sign in before favoriting
+   before_action :require_sign_in
+
+   def create
+     # find - post to favorite using `post_id` in `params`
+     # create - favorite for `current_user`, passing in `post` to establish associations for: user, post & favorite
+     post = Post.find(params[:post_id])
+     favorite = current_user.favorites.build(post: post)
+
+     if favorite.save
+       flash[:notice] = "Post favorited."
+     else
+       flash[:alert] = "Favoriting failed."
+     end
+
+     # redirect - user to post's show view
+     redirect_to [post.topic, post]
+   end
+
+   def destroy
+     post = Post.find(params[:post_id])
+     favorite = current_user.favorites.find(params[:id])
+
+     if favorite.destroy
+       flash[:notice] = "Post unfavorited."
+     else
+       flash[:alert] = "Unfavoriting failed."
+     end
+       redirect_to [post.topic, post]
+   end
+end
